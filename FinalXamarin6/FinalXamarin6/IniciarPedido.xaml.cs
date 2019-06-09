133b49e8aee9fc53a183255d3e75bae6564247d5
Victor Gonzalez Martinez <victor309710gonz@gmail.com>
2019-06-09 01:43:26 -0500

    Interfaces de la app, y algunos cambios para la navegación

---
+++ FinalXamarin6/FinalXamarin6/IniciarPedido.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FinalXamarin6
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class IniciarPedido : ContentPage
+	{
+		public IniciarPedido ()
+		{
+			InitializeComponent ();
+		}
+
+        private void BtnEnviar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string cliente = "";
+                string producto = "";
+                double importeTotal = 0;
+                string comentario = "";
+                DateTime fecha;
+
+                if (txtCliente.Text != "" && txtProducto.Text != "" && 
+                    txtImporteTotal.Text != "" && txtComentario.Text != "")
+                {
+                    cliente = txtCliente.Text;
+                    producto = txtProducto.Text;
+                    comentario = txtComentario.Text;
+                    importeTotal = double.Parse(txtImporteTotal.Text);
+                }
+                else
+                {
+                    DisplayAlert("Campos vacíos", "Por favor, llene los campos con la información solicitada", "Ok");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error:", ex.Message, "Ok");
+            }
+            
+        }
+    }
+}
